56df4fe6567310d427e9e2e47249be2a5e12d36f
AJ <programacion628@gmail.com>
2025-09-21 20:25:22 -0500

    Add files via upload
    El tipo double se usa para números decimales con alta precisión (64 bits)
     Ideal para cálculos científicos, financieros o mediciones exactas

---
+++ 2.VariablesYTiposDeDatos/DoubleBasico.java
@@ -0,0 +1,13 @@
+public class DoubleBasico 
+{
+    public static void main(String[] args) 
+	{
+        double peso = 65.789;
+		
+        System.out.print("Peso: " + peso);
+    }
+}
+
+
+// El tipo double se usa para números decimales con alta precisión (64 bits)
+        // Ideal para cálculos científicos, financieros o mediciones exactas
