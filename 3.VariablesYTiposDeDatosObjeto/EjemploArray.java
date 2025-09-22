72f4a8828082493127b9050a9f53d24ced05d92f
AJ <programacion628@gmail.com>
2025-09-21 20:50:20 -0500

    Add files via upload
    // Array almacena múltiples valores del mismo tipo
     //odos los elementos deben ser del mismo tipo (int, String, double, etc.).
    //Su tamaño es fijo: no se puede cambiar una vez creado
     
     //¿Por qué usar arrays?
    //Para organizar datos relacionados (por ejemplo, edades, nombres, temperaturas).
    //Para recorrer elementos con bucles (for, while).
    //Para ahorrar código en vez de declarar muchas variables individuales.

---
+++ 3.VariablesYTiposDeDatosObjeto/EjemploArray.java
@@ -0,0 +1,18 @@
+public class EjemploArray 
+{
+    public static void main(String[] args) 
+	{
+        int[] numeros = {10, 20, 30};
+		
+        System.out.print("Primer número: " + numeros[0]);
+    }
+}
+
+ // Array almacena múltiples valores del mismo tipo
+ //odos los elementos deben ser del mismo tipo (int, String, double, etc.).
+//Su tamaño es fijo: no se puede cambiar una vez creado
+ 
+ //¿Por qué usar arrays?
+//Para organizar datos relacionados (por ejemplo, edades, nombres, temperaturas).
+//Para recorrer elementos con bucles (for, while).
+//Para ahorrar código en vez de declarar muchas variables individuales.
