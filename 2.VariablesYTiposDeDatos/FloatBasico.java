cc004bba85793b47e2c6c1a04bc9f36b1f84bbed
AJ <programacion628@gmail.com>
2025-09-21 20:22:20 -0500

    Add files via upload
     El tipo float se usa para números decimales con precisión simple (32 bits)
      Ideal para representar valores como precios, temperaturas, porcentajes, etc.

---
+++ 2.VariablesYTiposDeDatos/FloatBasico.java
@@ -0,0 +1,13 @@
+public class FloatBasico 
+{
+    public static void main(String[] args) 
+	{
+        float temperatura = 36.5f;
+		
+        System.out.print("Temperatura: " + temperatura);
+    }
+}
+
+
+ // El tipo float se usa para números decimales con precisión simple (32 bits)
+        // Ideal para representar valores como precios, temperaturas, porcentajes, etc.
