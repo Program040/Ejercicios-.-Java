b40b7841af889dc669ad8b60a7110b1dc9d83c2a
AJ <programacion628@gmail.com>
2025-09-21 20:37:14 -0500

    Add files via upload
       El tipo boolean se usa para representar valores lógicos: true (verdadero) o false (falso)
          Ideal para condiciones, decisiones y estados lógicos

---
+++ 2.VariablesYTiposDeDatos/BooleanBasico.java
@@ -0,0 +1,12 @@
+public class BooleanBasico 
+{
+    public static void main(String[] args) 
+	{
+        boolean aprobado = true;
+		
+        System.out.print("¿Aprobado?: " + aprobado);
+    }
+}
+
+// El tipo boolean se usa para representar valores lógicos: true (verdadero) o false (falso)
+        // Ideal para condiciones, decisiones y estados lógicos
