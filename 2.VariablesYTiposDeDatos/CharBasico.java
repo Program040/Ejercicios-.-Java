539e7ac5084e37820d6662ccf591731bc597916b
AJ <programacion628@gmail.com>
2025-09-21 20:28:01 -0500

    Add files via upload
    El tipo char se usa para almacenar un solo carácter Unicode (letra, número, símbolo)

---
+++ 2.VariablesYTiposDeDatos/CharBasico.java
@@ -0,0 +1,12 @@
+public class CharBasico 
+{
+    public static void main(String[] args) 
+	{
+        char inicial = 'P';
+		
+        System.out.print("Inicial: " + inicial);
+    }
+}
+
+
+// El tipo char se usa para almacenar un solo carácter Unicode (letra, número, símbolo)
