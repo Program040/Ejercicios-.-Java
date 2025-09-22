e27372eb8707c106e22ff815fdf3d4b6f1bf1275
AJ <programacion628@gmail.com>
2025-09-21 20:46:07 -0500

    Add files via upload
    String se usa para almacenar texto

---
+++ 3.VariablesYTiposDeDatosObjeto/EjemploString.java
@@ -0,0 +1,11 @@
+public class EjemploString 
+{
+    public static void main(String[] args) 
+	{
+        String nombre = "Programación";
+		
+        System.out.print("Nombre: " + nombre);
+    }
+}
+
+ // String se usa para almacenar texto
