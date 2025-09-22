cc5013afa109ad69496c6549e78f0a68f2fef41b
AJ <programacion628@gmail.com>
2025-09-21 20:20:06 -0500

    Add files via upload
     El tipo long se usa para números enteros muy grandes (rango: ±9 cuatrillones)
      Ideal para contar cosas como población mundial, distancia entre planetas, etc.

---
+++ 2.VariablesYTiposDeDatos/LongBasico.java
@@ -0,0 +1,12 @@
+public class LongBasico 
+{
+    public static void main(String[] args) 
+	{
+        long distancia = 150000000000L;
+		
+        System.out.print("Distancia: " + distancia);
+    }
+}
+
+// El tipo long se usa para números enteros muy grandes (rango: ±9 cuatrillones)
+        // Ideal para contar cosas como población mundial, distancia entre planetas, etc.
