fa566370bd6e011274d19f484c878a22220ed7f9
AJ <programacion628@gmail.com>
2025-09-21 21:27:51 -0500

    Add files via upload

---
+++ 4.Condicionales(if,else,else if)/EjCondicionalelseif.java
@@ -0,0 +1,10 @@
+int nota = 85;
+
+if (nota >= 90) 
+{
+    System.out.println("Excelente");
+} else if (nota >= 70) {
+    System.out.println("Aprobado");
+} else {
+    System.out.println("Reprobado");
+}
