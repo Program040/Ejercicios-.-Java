417ed12d632cea785ecd8101d17180d57669f37b
AJ <programacion628@gmail.com>
2025-09-21 20:11:40 -0500

    Add files via upload
    El tipo short se usa para números enteros pequeños (rango: -32,768 a 32,767)
    Es útil cuando queremos ahorrar memoria en grandes cantidades de datos numéricos

---
+++ 2.VariablesYTiposDeDatos/ShortBasico.java
@@ -0,0 +1,12 @@
+public class ShortBasico 
+{
+    public static void main(String[] args) 
+	{
+        short altura = 170;
+		
+        System.out.print("Altura: " + altura);
+    }
+}
+
+ // El tipo short se usa para números enteros pequeños (rango: -32,768 a 32,767)
+        // Es útil cuando queremos ahorrar memoria en grandes cantidades de datos numéricos
